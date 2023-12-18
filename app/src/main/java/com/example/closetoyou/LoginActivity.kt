f0c77602e7dc9b40809e9a6c989a75f02b0d4016
Filip7243 <filip7243@gmail.com>
2023-12-18 18:06:14 +0100

    login xml

---
+++ app/src/main/java/com/example/closetoyou/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.closetoyou
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class LoginActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // zapytaj o wszystkie możliwe uprawnienia,
+        //
+    }
+}
